b9e9cadaedf8acfd77f55ee5f70476439df75579
shailabrazil <sheilaadesina85@gmail.com>
2018-10-24 17:44:49 +1100

    identation

--- js/app.js
+++ js/app.js
@@ -12,28 +12,27 @@ videos.ontimeupdate = () => {
 
 //Inside the event listener, loop over each span element.
 //loop then span element
-  for (let i = 0; i <= spans.length; i ++) {
+   for (let i = 0; i <= spans.length; i ++) {
 //Inside the loop, use a conditional to see if the video’s current time is
-    const datas = videos.currentTime;
+       const datas = videos.currentTime;
 // If the conditions are met, set the span’s color property to the highlighted
 // color of your choice, and if not, set the span’s color back to its initial color.
-    if (datas > spans[i].getAttribute("data-start")
-    && datas < spans[i].getAttribute("data-end")) {
-      spans[i].style.color = "#FFA500";
-   
+       if (datas > spans[i].getAttribute("data-start")
+        && datas < spans[i].getAttribute("data-end")) {
+         spans[i].style.color = "#FFA500";
     }
-    else {
-      spans[i].style.color = "black";
-     
+         else {
+          spans[i].style.color = "black";
+
     }
   }
 };
 
 // walk around the video
 const spanProperty = document.querySelectorAll("p span");
-for(let i = 0; i < spanProperty.length; i += 1) {
-  spanProperty[i].addEventListener("click", function(event) {
-    videos.currentTime = event.target.getAttribute("data-start");
-    videos.play();
+     for(let i = 0; i < spanProperty.length; i += 1) {
+     spanProperty[i].addEventListener("click", function(event) {
+     videos.currentTime = event.target.getAttribute("data-start");
+     videos.play();
   })
 }
